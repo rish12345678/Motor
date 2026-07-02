6ebce864af766fabcdce2f93dd134964ba568726
Rishab Madhugiri <madhugiri.r@northeastern.edu>
2026-07-02 01:00:29 -0700

    Set GPIOA pins 5 6 7 to route to SPI1 peripheral

--- Src/main.c
+++ Src/main.c
@@ -1,4 +1,5 @@
-#include "../drivers/Inc/mem_defs.h"
+#include "../Inc/stm32l476xx.h"
+
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -11,6 +12,9 @@ int main(void)
 {
 	// Turn on clock for GPIOA and SPI1
 	void clock_init(void);
+
+	// Set mode for GPIOA (AF)
+	void GPIO_mode_init(void);
 
 
 
@@ -92,4 +96,16 @@ void clock_init(void) {
 	// Set both bits to one enabling clock
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; // Gate clock to GPIO (A)
 	RCC->APB2ENR1 |= RCC_APB2ENR_SPI1EN; // Gate clock to SPI1
+}
+
+void GPIO_mode_init(void) {
+	// Set GPIO Port A pins 5, 6, 7 to AF - Part of SPI bus now
+	GPIOA->MODER &= ~(GPIO_MODER_MODE5 | GPIO_MODER_MODE6 | GPIO_MODER_MODE7);
+	GPIOA->MODER |= (GPIO_MODER_MODE5_1 | GPIO_MODER_MODE6_1 | GPIO_MODER_MODE7_1);
+
+	// Set these four pins to belong to the SPI1 Peripheral (Will hardcode chip select line)
+	GPIOA->AFR[0] &= ~(0xF << GPIO_AFRL_AFSEL5_Pos | 0xF << GPIO_AFRL_AFSEL6_Pos | 0xF << GPIO_AFRL_AFSEL7_Pos); // Clear for pins 5 6 7
+
+	GPIOA->AFR[0] |= ( (GPIO_AFRL_AFSEL5_0 | GPIO_AFRL_AFSEL5_2) | (GPIO_AFRL_AFSEL6_0 | GPIO_AFRL_AFSEL6_2) | (GPIO_AFRL_AFSEL7_0 | GPIO_AFRL_AFSEL7_2));
+
 }
